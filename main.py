378125d23a6e2f32db4820fbb62b318765b83f9e
esquaredsec <16594866+esquaredsec@users.noreply.github.com>
2026-06-10 19:24:16 -0400

    Creating a sorted list function for reporting

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from stats import word_counter, char_counter
+from stats import word_counter, char_counter, chars_dict_to_sorted_list
 
 def main():
     file_path = "books/frankenstein.txt"
     text = get_book_text(file_path)
     total_words = word_counter(text)
     char_dict_count = char_counter(text)
+    char_sorted_list = chars_dict_to_sorted_list(char_dict_count)
     print(f"Found {total_words} total words")
     print(char_dict_count)
+    print(char_sorted_list)
 
-def get_book_text(file_path):
+def get_book_text(file_path: str):
     with open(file_path, 'r', encoding='utf-8') as f:
         return f.read()
 
--- stats.py
+++ stats.py
@@ -1,11 +1,12 @@
-def word_counter(text):
-    word_list = text
-    words = len(word_list.split())
+def word_counter(text: str):
+    word_list: str = text
+    words: int = len(word_list.split())
     return words
 
-def char_counter(text):
-    char_dict = {}
-    char_counts = text.lower()
+def char_counter(text: str) -> dict:
+    char_dict: dict = {}
+    char_counts: str = text.lower()
+
     for char in char_counts:
         if char in char_dict:
             char_dict[char] += 1
@@ -13,3 +14,14 @@ def char_counter(text):
             char_dict[char] = 1
 
     return char_dict
+
+def sort_on(char_dict: tuple[str, int]) -> int:
+    return char_dict[1]
+
+def chars_dict_to_sorted_list(char_dict: dict[str, int]) -> list[tuple[str, int]]:
+    sorted_list = []
+    for k, v in char_dict.items():
+        sorted_list.append((k,v))
+    proper_sorted_list = sorted(sorted_list, reverse=True, key=sort_on)
+    return proper_sorted_list
+
